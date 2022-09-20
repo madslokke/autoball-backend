4a459dd5f7ef1ce54f9a59bf544942488d2cf31d
Mads Mikkelsen <madslokke@gmail.com>
2022-09-20 20:46:05 +0200

    Added migration

---
+++ database/migrations/2022_09_20_182535_add_team_id_to_players.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Team;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->foreignIdFor(Team::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            //
+        });
+    }
+};
